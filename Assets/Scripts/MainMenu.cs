d650bc3ecbd8418210bcf33604d84f33d4bfa667
Sarah Leventhal <sul2102@barnard.edu>
2020-04-21 01:14:35 -0400

    Style changes to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,12 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Learn()
+    {
+        SceneManager.LoadScene("Learn");
     }
 
     public void EndGame()
