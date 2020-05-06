5f4d260cb8cbfdbd339fa5330af202f10a5e294d
Sarah Leventhal <sul2102@barnard.edu>
2020-05-06 15:18:51 -0400

    isolate which handle should be manipulated by the user under each tab

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -20,6 +20,8 @@ public class Controller2D : MonoBehaviour
     public GameObject panelA;
     public GameObject panelF;
     public GameObject paneladd;
+    private GameObject aH;
+    private GameObject fH;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +29,10 @@ public class Controller2D : MonoBehaviour
         wavePrefab.SetActive(false);
         allWaves = new List<GameObject>();
         createNewWave();
+        aH = GameObject.FindWithTag("amplitudeHandle");
+        fH = GameObject.FindWithTag("frequencyHandle");
+        aH.SetActive(true);
+        fH.SetActive(false);
     }
 
     // Update is called once per frame
@@ -42,17 +48,12 @@ public class Controller2D : MonoBehaviour
                 Debug.Log("touch phase is stationary or moved");
                 RaycastHit hit;
                 Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.GetTouch(0).position);
-                /*Plane plane = new Plane(Vector3.up, transform.position);
-                float distance = 0;*/
                 if (Physics.Raycast(ray, out /*distance*/hit))
                 {
                     Debug.Log("a hit was made");
                     obj = hit.transform;
                     offSet = obj.position - hit.point;
-                    dist = (ray.origin - hit.point).magnitude;
-                    /* Vector3 pos = ray.GetPoint(distance);*/
-                    /*Vector3 pos = new Vector3(touch.position.x, touch.position.y, hit.transform.position.z);*/
-                    /*transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);*/     
+                    dist = (ray.origin - hit.point).magnitude;    
                 }
 
                 if (obj)
@@ -73,6 +74,11 @@ public class Controller2D : MonoBehaviour
                     else if (obj.name.Split('_')[0] == "waveHandle")
                     {
                         Debug.Log("WAVEHANDLE _______________________HANDLE");
+                        pos.x = obj.position.x;
+                        if (pos.y > 5)
+                            pos.y = 5;
+                        if (pos.y < -6)
+                            pos.y = -6;
                         obj.GetComponent<SineController>().SetPosition(pos);
                     }
                 }
@@ -105,24 +111,47 @@ public class Controller2D : MonoBehaviour
         prevSelected.tag = "buttonTab";
         button.GetComponent<Image>().sprite = selectedButton;
         button.tag = "selected";
+        /*GameObject aH = GameObject.FindWithTag("amplitudeHandle");
+        GameObject fH = GameObject.FindWithTag("frequencyHandle");*/
 
-        if(button.name == "Amplutude_Button")
+        if (button.name == "Amplutude_Button")
         {
             panelA.SetActive(true);
             panelF.SetActive(false);
             paneladd.SetActive(false);
+            aH.SetActive(true);
+            fH.SetActive(false);
+            /*aH.GetComponent<CapsuleCollider>().enabled = true;
+            aH.GetComponent<MeshRenderer>().enabled = true;
+            fH.GetComponent<CapsuleCollider>().enabled = false;
+            fH.GetComponent<MeshRenderer>().enabled = false;*/
+
         }
         else if(button.name == "Frequency_Button")
         {
             panelF.SetActive(true);
             panelA.SetActive(false);
             paneladd.SetActive(false);
+            aH.SetActive(false);
+            fH.SetActive(true);
+            /*aH.GetComponent<CapsuleCollider>().enabled = false;
+            aH.GetComponent<MeshRenderer>().enabled = false;
+            fH.GetComponent<CapsuleCollider>().enabled = true;
+            fH.GetComponent<MeshRenderer>().enabled = true;*/
         }
         else if(button.name == "ADD_Button")
         {
             paneladd.SetActive(true);
             panelA.SetActive(false);
             panelF.SetActive(false);
+            GameObject wave = GameObject.FindWithTag("wave");
+            wave.GetComponent<SphereCollider>().enabled = true;
+            aH.SetActive(true);
+            fH.SetActive(true);
+            /*aH.GetComponent<CapsuleCollider>().enabled = true;
+            aH.GetComponent<MeshRenderer>().enabled = true;
+            fH.GetComponent<CapsuleCollider>().enabled = true;
+            fH.GetComponent<MeshRenderer>().enabled = true;*/
         }
     }
 }
