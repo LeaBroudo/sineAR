3b9950298ecd4ed11198b47b32a8e77f7cc01033
Sarah Leventhal <sul2102@barnard.edu>
2020-05-11 18:45:46 -0400

    made the buttons appear and disappear as the panels are passed through for the tutorial

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -5,9 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour
 {
-    /*GameObject activePanel;*/
+    public GameObject Select;
+    public GameObject ChangeMode;
+    public GameObject delete;
+    public GameObject Panel;
+
     public void ChangePanel(GameObject nextPanel)
     {
+        if(nextPanel.name == "1_addwave")
+        {
+            Select.SetActive(false);
+            ChangeMode.SetActive(false);
+            delete.SetActive(false);
+            Panel.SetActive(false);
+        }
+
+        else if(nextPanel.name == "2_dragtoscene")
+        {
+            Select.SetActive(false);
+            Panel.SetActive(true);
+        }
+
+        else if (nextPanel.name == "3_manipulate") { Select.SetActive(true); }
+        
+        else if (nextPanel.name == "4_combinewaves") { ChangeMode.SetActive(false); }
+        
+        else if (nextPanel.name == "5_conductormode")
+        {
+            ChangeMode.SetActive(true);
+            delete.SetActive(false);
+        }
+
+        else if (nextPanel.name == "6_deletewave") { delete.SetActive(true); }
+        
+        /*else if (nextPanel.name == "7_done"){}*/
+
         nextPanel.SetActive(true);
         gameObject.SetActive(false);
     }
